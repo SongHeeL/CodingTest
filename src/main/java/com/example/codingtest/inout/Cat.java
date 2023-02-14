3b4c564e81fcd2bceb681875cc76538bed70f09c
SongHeeL <108327480+SongHeeL@users.noreply.github.com>
2023-02-14 10:36:58 +0900

    commit

---
+++ src/main/java/com/example/codingtest/inout/Cat.java
@@ -0,0 +1,10 @@
+package com.example.codingtest.inout;
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ src/main/java/com/example/codingtest/inout/Sprout.java
@@ -0,0 +1,12 @@
+package com.example.codingtest.inout;
+
+public class Sprout {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
